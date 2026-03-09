1653f82a798974af4fbd133062a89c55d2ed0292
Renzo <renzo.quispe.va@tecsup.edu.pe>
2026-03-09 18:34:39 -0500

    chore: ignore dist build output

--- dist/modules/companies/presentation/companyRouter.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const CompanyController_1 = require("./CompanyController");
-const authMiddleware_1 = require("../../../shared/infrastructure/auth/authMiddleware");
-const router = (0, express_1.Router)();
-const controller = new CompanyController_1.CompanyController();
-router.get("/:companyId", authMiddleware_1.authMiddleware, authMiddleware_1.requireStoreAdmin, (0, authMiddleware_1.requireSameCompanyFromParam)("companyId"), controller.getById);
-exports.default = router;
--- dist/modules/users/presentation/sellersRouter.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const SellersController_1 = require("./SellersController");
-const authMiddleware_1 = require("../../../shared/infrastructure/auth/authMiddleware");
-const router = (0, express_1.Router)();
-const controller = new SellersController_1.SellersController();
-router.post("/", authMiddleware_1.authMiddleware, authMiddleware_1.requireStoreAdmin, controller.create);
-exports.default = router;
