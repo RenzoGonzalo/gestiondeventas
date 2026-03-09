9739279ef7217495529d782a9926b0eeedbfe9eb
Renzo <renzo.quispe.va@tecsup.edu.pe>
2026-03-09 18:34:39 -0500

    chore: ignore dist build output

--- backend/dist/modules/users/presentation/superRouter.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const SuperAdminController_1 = require("./SuperAdminController");
-const authMiddleware_1 = require("../../../shared/infrastructure/auth/authMiddleware");
-const router = (0, express_1.Router)();
-const controller = new SuperAdminController_1.SuperAdminController();
-router.post("/companies", authMiddleware_1.authMiddleware, authMiddleware_1.requireSuperAdmin, controller.provisionCompany);
-exports.default = router;
